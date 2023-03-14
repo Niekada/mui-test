3c75dad726f226969a6a106d8e6342f3700700d0
Niekada <erik.tankeliun@gmail.com>
2023-03-14 21:52:21 +0200

    navbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,104 +13,102 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import NavBar from '../NavBar/NavBar';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
-export default function Album() {
+const Home = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Here you can find my photo album
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
+    <div>
+        <NavBar />
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>
+            <Box
+            sx={{
+                bgcolor: 'background.paper',
+                pt: 8,
+                pb: 6,
+            }}
             >
-              Niekada's photos
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              All photos were taken last year, on my journay around the World
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Follow me on Instagram</Button>
-              <Button variant="outlined">Follow me on Facebook</Button>
-            </Stack>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+            <Container maxWidth="sm">
+                <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
                 >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Paris
-                    </Typography>
-                    <Typography>
-                      I lived in Paris are for 6 months and recommend everyone not just visit the main city, but try traveling around
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Hope you have a great day
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Please check my album next Month!
-        </Typography>
-      </Box>
-    </ThemeProvider>
+                Niekada's photos
+                </Typography>
+                <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                All photos were taken last year, on my journay around the World
+                </Typography>
+                <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+                >
+                <Button variant="contained">Follow me on Instagram</Button>
+                <Button variant="outlined">Follow me on Facebook</Button>
+                </Stack>
+            </Container>
+            </Box>
+            <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+                {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                    <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                    >
+                    <CardMedia
+                        component="img"
+                        sx={{
+                        // 16:9
+                        pt: '56.25%',
+                        }}
+                        image="https://source.unsplash.com/random"
+                        alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                        Paris
+                        </Typography>
+                        <Typography>
+                        I lived in Paris are for 6 months and recommend everyone not just visit the main city, but try traveling around
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View</Button>
+                        <Button size="small">Edit</Button>
+                    </CardActions>
+                    </Card>
+                </Grid>
+                ))}
+            </Grid>
+            </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+            <Typography variant="h6" align="center" gutterBottom>
+            Hope you have a great day
+            </Typography>
+            <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+            >
+            Please check my album next Month!
+            </Typography>
+        </Box>
+        </ThemeProvider>
+    </div>
   );
-}
+};
+
+export default Home;
